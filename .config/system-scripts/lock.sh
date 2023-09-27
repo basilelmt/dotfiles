c14284bcce1a1f0d788f796fff4a57fa3d2d3c09
Basile Lamotte <basile.lamotte@epitech.eu>
2023-09-27 13:53:56 +0200

    update .config

---
+++ .config/system-scripts/lock.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+swaylock \
+	--screenshots \
+	--effect-blur 7x5 \
+	--indicator-thickness 14 \
+	--effect-vignette 0.5:0.5 \
+	--grace 2 \
+  --indicator-idle-visible \
+	--fade-in 0.2
